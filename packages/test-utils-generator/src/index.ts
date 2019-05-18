3ed4db475f493139430833811d9a6809bfd3b7b8
sigoden <sigoden@gmail.com>
2019-05-18 20:39:09 +0800

    fix(test-utils-generator): parseApis

--- packages/test-utils-generator/src/index.ts
+++ packages/test-utils-generator/src/index.ts
@@ -5,5 +5,5 @@ import { loadFixtureJSON5 } from "@jiasuyun/apier-test-utils";
 export function loadApiers(name: string): apier.Apier[] {
   const input = loadFixtureJSON5(name);
   const parser = new Parser();
-  return apier.parse(input, parser);
+  return apier.parse(input, parser).apiers;
 }
