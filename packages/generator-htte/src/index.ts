b1ef9652847b429f04dba1e0f37da758548535ba
sigoden <sigoden@gmail.com>
2019-05-29 14:44:27 +0800

    feat: generator htte split handlers and defines

--- packages/generator-htte/src/index.ts
+++ packages/generator-htte/src/index.ts
@@ -1,12 +1,22 @@
 import { colonToCurlybrace } from "@jiasuyun/apier-utils";
 import { Apier } from "@jiasuyun/apier";
-import { ApierRawReq, ApierRawRes } from "@jiasuyun/apier-parser-base";
+import { ApierRawReq, ApierRawRes, Method } from "@jiasuyun/apier-parser-base";
 import lget from "lodash/get";
 import merge from "lodash/merge";
 
-export type GeneratorResult = TestItem;
+export interface GeneratorResult {
+  define: Define;
+  test: TestItem;
+}
 
 export type TestItem = TestGroup | TestUnit;
+
+interface Define {
+  [k: string]: {
+    method: Method;
+    url: string;
+  };
+}
 
 interface TestGroup {
   describe: string;
@@ -15,10 +25,8 @@ interface TestGroup {
 
 interface TestUnit {
   describe: string;
-  req: {
-    method: string;
-    url: string;
-  } & ApierRawReq;
+  include: string;
+  req: ApierRawReq;
   res: ApierRawRes;
 }
 
@@ -33,25 +41,31 @@ export default class Generator {
     } = apier;
     const meta = apier.comment.retriveMeta();
     const summary = apier.comment.retrive().val("summary", name) as string;
-    const defaultUnit = {
+    const test = {
       describe: summary,
-      req: { method, url: colonToCurlybrace(url), ...req },
+      include: name,
+      req: req,
       res: res[0]
     };
+    const define = { [name]: { method, url: colonToCurlybrace(url) } };
     let units = loadUnits(meta);
     if (units.length === 0) {
-      this.value = defaultUnit;
+      this.value = { test, define };
       return;
     }
     this.value = {
-      describe: summary,
-      units: meta.htte.units.map((unit, index) => {
-        if (index === 0) {
-          return merge({}, defaultUnit, unit);
-        }
-        unit.metadata = { skip: true };
-        return unit;
-      })
+      test: {
+        describe: summary,
+        units: meta.htte.units.map((unit, index) => {
+          if (index === 0) {
+            return merge(test, unit);
+          }
+          unit.include = name;
+          unit.metadata = { skip: true };
+          return unit;
+        })
+      },
+      define
     };
   }
 }
